48a2189b9431ec5d3ab3a64571c3b68652856340
ndikrulil <developerroot9@gmail.com>
2024-11-01 09:45:15 +0700

    a

--- src/components/blocks/Footer/Footer.tsx
+++ src/components/blocks/Footer/Footer.tsx
@@ -7,7 +7,7 @@ export const Footer = memo(() => {
   return (
     <footer className="bg-zinc-800 text-white text-center py-8">
       <Container>
-        <p>Merdeka Square, Jalan Lapangan Monas, Gambir, Kecamatan Gambir, Kota Jakarta Pusat <a href="https://www.google.com/maps/place/Monumen+Nasional/@-6.1745491,106.8243148,565m/data=!3m1!1e3!4m10!1m2!2m1!1smonas!3m6!1s0x2e69f5d2e764b12d:0x3d2ad6e1e0e9bcc8!8m2!3d-6.1753924!4d106.8271528!15sCgVtb25hc1oHIgVtb25hc5IBCG1vbnVtZW504AEA!16zL20vMDNxN2hz?entry=ttu&g_ep=EgoyMDI0MTAyOS4wIKXMDSoASAFQAw%3D%3D"  style="color:##4a81ff;">Google Maps</a></p>
+        <p>Merdeka Square, Jalan Lapangan Monas, Gambir, Kecamatan Gambir, Kota Jakarta Pusat <a href="https://maps.app.goo.gl/NDPvPsYfrRjvyaQF9"  style="color:##4a81ff">Google Maps</a></p>
         <p>AutoDealer © {actualYear}</p>
       </Container>
     </footer>
