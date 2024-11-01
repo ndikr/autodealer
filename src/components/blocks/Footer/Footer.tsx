cb021f24a519c642e0c6aeb97b443a6750b1ec49
ndikrulil <developerroot9@gmail.com>
2024-11-01 09:46:52 +0700

    a

--- src/components/blocks/Footer/Footer.tsx
+++ src/components/blocks/Footer/Footer.tsx
@@ -7,7 +7,7 @@ export const Footer = memo(() => {
   return (
     <footer className="bg-zinc-800 text-white text-center py-8">
       <Container>
-        <p>Merdeka Square, Jalan Lapangan Monas, Gambir, Kecamatan Gambir, Kota Jakarta Pusat <a href="https://maps.app.goo.gl/NDPvPsYfrRjvyaQF9"  style="color:##4a81ff">Google Maps</a></p>
+        <p>Merdeka Square, Jalan Lapangan Monas, Gambir, Kecamatan Gambir, Kota Jakarta Pusat <a href="https://maps.app.goo.gl/NDPvPsYfrRjvyaQF9"  style="color:#4a81ff">Google Maps</a></p>
         <p>AutoDealer © {actualYear}</p>
       </Container>
     </footer>
